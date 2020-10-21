023252ff57fe391e2d9ad53e07f1111c27a619d6
gurelbs <guri240@gmail.com>
2020-10-21 19:01:55 +0300

    Updates

---
+++ precache-manifest.bd280e5850b675a721861d97bbb4a627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e33a1468d2873ac0c2d04da2906faa",
+    "url": "/recognition/index.html"
+  },
+  {
+    "revision": "b8d791b1a9fc93d9c514",
+    "url": "/recognition/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "b8d791b1a9fc93d9c514",
+    "url": "/recognition/static/js/2.80122eab.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/recognition/static/js/2.80122eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29321b336b78c92cc0d",
+    "url": "/recognition/static/js/main.4575d0a1.chunk.js"
+  },
+  {
+    "revision": "42a0695fec66f63c51de",
+    "url": "/recognition/static/js/runtime-main.63f9aa37.js"
+  }
+]);
